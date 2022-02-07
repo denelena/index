9813e82599c57c56c962c0d6916a281a1b84e8fe
Елена Денисова <E-denisova@yandex.ru>
2022-02-07 14:10:11 +0300

    Code formatting added

--- src/com/company/BmiService.java
+++ src/com/company/BmiService.java
@@ -1,9 +1,7 @@
 package com.company;
 
-public class BmiService
-{
-    public float calculate(float weight_in_kg, float height_in_m)
-    {
-        return weight_in_kg / (height_in_m * height_in_m);
+public class BmiService {
+    public float calculate(float weightInKg, float heightInM) {
+        return weightInKg / (heightInM * heightInM);
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,15 +1,13 @@
 package com.company;
 
-public class Main
-{
-    public static void main(String[] args)
-    {
-        float massa = 65f; // килограммов
-        float rost = 1.75f; // метров
+public class Main {
+    public static void main(String[] args) {
+        float massaKg = 65f; // килограммов
+        float rostMeters = 1.75f; // метров
 
         BmiService bmiCalculator = new BmiService();
 
-        float bmIndex = bmiCalculator.calculate(massa, rost);
+        float bmIndex = bmiCalculator.calculate(massaKg, rostMeters);
         System.out.println("BMI: " + bmIndex);
     }
 }
